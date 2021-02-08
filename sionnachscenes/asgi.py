bc655a066948e44e6d26117c112c3b7905165472
Gwen-of-lynn <Gwen.of.lynn@gmail.com>
2021-02-08 08:41:18 +0000

    Changed name from SionnachScenes to sionnachscenes as troubleshooting step for environment variables in gitpod

--- sionnachscenes/asgi.py
+++ sionnachscenes/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for SionnachScenes project.
+ASGI config for sionnachscenes project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SionnachScenes.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sionnachscenes.settings')
 
 application = get_asgi_application()
--- sionnachscenes/wsgi.py
+++ sionnachscenes/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for SionnachScenes project.
+WSGI config for sionnachscenes project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SionnachScenes.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sionnachscenes.settings')
 
 application = get_wsgi_application()
